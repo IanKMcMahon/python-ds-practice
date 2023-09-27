6c6591dd6e2dd8be156d4c9919d15d82f7add6c0
Ian McMahon <iankmcmahon@gmail.com>
2023-09-26 20:32:23 -0700

    titleize

--- 27_titleize/titleize.py
+++ 27_titleize/titleize.py
@@ -7,3 +7,6 @@ def titleize(phrase):
         >>> titleize('oNLy cAPITALIZe fIRSt')
         'Only Capitalize First'
     """
+    phrase = phrase.lower()
+
+    return [word.capitalize() for word in phrase]
