fc64eb101ac5f5f3790467b278b18e951822022c
Ian McMahon <iankmcmahon@gmail.com>
2023-09-26 20:24:54 -0700

    vowel_count

--- 26_vowel_count/vowel_count.py
+++ 26_vowel_count/vowel_count.py
@@ -3,7 +3,16 @@ def vowel_count(phrase):
 
         >>> vowel_count('rithm school')
         {'i': 1, 'o': 2}
-        
-        >>> vowel_count('HOW ARE YOU? i am great!') 
+
+        >>> vowel_count('HOW ARE YOU? i am great!')
         {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1}
     """
+    vowels = 'aeiou'
+    phrase = phrase.lower()
+    counter = {}
+
+    for letter in phrase:
+        if letter in vowels:
+            counter[letter] = counter.get(letter, 0) + 1
+
+    return counter
